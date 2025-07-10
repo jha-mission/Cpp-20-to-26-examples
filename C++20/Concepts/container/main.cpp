d1d5a92838b6fce539b3f1472916548141721376
Johann Hagler <jha@mission-embedded.com>
2025-07-10 19:57:06 +0200

    update concepts example with sfinae version

--- C++20/Concepts/container/main.cpp
+++ C++20/Concepts/container/main.cpp
@@ -10,16 +10,52 @@ concept HasPushBack = requires(Collection c, typename Collection::value_type v) 
   c.push_back(v);
 };
 
-template<typename Collection, typename T>
-requires HasPushBack<Collection>
-void add(Collection& collection, T value) {
+// template<HasPushBack Collection, typename T>
+// void add(Collection& collection, T value) {
+//   collection.push_back(value);
+// }
+
+// template<typename Collection, typename T>
+// void add(Collection& collection, T value) {
+//   collection.insert(value);
+// }
+
+void add(HasPushBack auto& collection, auto value) {
   collection.push_back(value);
 }
 
-template<typename Collection, typename T>
-void add(Collection& collection, T value) {
+void add(auto& collection, auto value) {
   collection.insert(value);
 }
+
+// old school (SFINAE)
+// Helper trait to detect push_back
+// template<typename C, typename T>
+// class has_push_back {
+// private:
+//     template<typename U>
+//     static auto test(int) -> decltype(std::declval<U>().push_back(std::declval<T>()), std::true_type());
+
+//     template<typename>
+//     static std::false_type test(...);
+// public:
+//     static constexpr bool value = decltype(test<C>(0))::value;
+// };
+
+// // Enable only if Collection has push_back(T)
+// template<typename Collection, typename T>
+// typename std::enable_if<has_push_back<Collection, T>::value, void>::type
+// add(Collection& collection, T value) {
+//     collection.push_back(value);
+// }
+// template<typename Collection, typename T>
+// typename std::enable_if<
+//     !has_push_back<Collection, T>::value,
+//     void>::type
+// add(Collection& collection, T value) {
+//     collection.insert(value);
+// }
+
 
 //-------------------------------------------------------------------------------
 int main() {
