a5f2af0dc4ddea43181abfbd46b08f2905970887
Johann Hagler <jha@mission-embedded.com>
2025-09-03 18:48:43 +0200

    add hello world word count

---
+++ C++20/modules/hello_world_word_count/main.cpp
@@ -0,0 +1,11 @@
+
+// count with (213) and without modules (1118756)
+// preprocessing only: -E
+// g++ -I. -std=c++20 -E main.cpp | wc -c
+
+// #include <iostream>
+import <iostream>;
+
+int main() {
+    std::cout << "Hello Modular World!\n";
+}
