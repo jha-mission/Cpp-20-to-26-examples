e373ed3362bca33054ceb98b5caa42586686024a
Johann Hagler <jha@mission-embedded.com>
2025-09-08 11:52:52 +0200

    deducing this example

--- C++23/deducing_this/simple/main.cpp
+++ C++23/deducing_this/simple/main.cpp
@@ -1,24 +1,49 @@
+// Example from Deducing this P0847R7 - open-std.org/jtc1/sc22/wg21/docs/papers/2021/p0847r7.html#motivation
+
 #include <iostream>
+#include <stdexcept>
+#include <utility>
 
-struct Point {
-  int x, y;
+template <typename T>
+class my_optional {
+  T m_value;
 
-  // Deducing this: 'auto' makes this a template on cv/ref qualifiers
-  auto operator+=(this Point& self, const Point& other) {
-    self.x += other.x;
-    self.y += other.y;
-    return self;
-  }
+ public:
+  my_optional() = default;
+  my_optional(T v) : m_value(std::move(v)) {}
 
-  void print(this const Point& self) {
-    std::cout << "(" << self.x << ", " << self.y << ")\n";
+  // // 1. lvalue, non-const
+  // constexpr T& value() & {
+  //   return m_value;
+  // }
+
+  // // 2. lvalue, const
+  // constexpr const T& value() const& {
+  //   return m_value;
+  // }
+
+  // // 3. rvalue, non-const
+  // constexpr T&& value() && {
+  //   return std::move(m_value);
+  // }
+
+  // // 4. rvalue, const
+  // constexpr const T&& value() const&& {
+  //   return std::move(m_value);
+  // }
+
+  // Single function replaces all 4 overloads
+  template <typename Self>
+  constexpr auto&& value(this Self&& self) {
+    return std::forward<Self>(self).m_value;
   }
 };
 
 int main() {
-  Point p1{1, 2};
-  Point p2{3, 4};
+  my_optional<int> o1(42);
+  const my_optional<int> o2(7);
 
-  p1 += p2;
-  p1.print();  // Output: (4, 6)
+  std::cout << o1.value() << "\n";                   // lvalue, non-const
+  std::cout << o2.value() << "\n";                   // lvalue, const
+  std::cout << my_optional<int>(9).value() << "\n";  // rvalue
 }
